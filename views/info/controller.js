ff3d3704053b4bcd9262d53dd0d19af228fac3c6
Alan Tsai <alanalan216@gmail.com>
2016-07-27 09:37:33 -0700

    Update controller.js

--- views/info/controller.js
+++ views/info/controller.js
@@ -1,4 +1,4 @@
-angular.module('myApp.place', ['ngRoute'])
+angular.module('myApp.info', ['ngRoute'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('info', {
 			templateUrl: 'views/info/info.html',
